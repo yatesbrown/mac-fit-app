67597806d01983676a36c370c56f7ce68027d6f2
Mac Cassidy <maccassidy1995@gmail.com>
2016-08-04 14:36:54 -0400

    changing the homepage to a login page and moving what i've got so far onto another page called workoutpage, but im keeping it in the controller

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,13 @@
 class PageController < ApplicationController
-  def homepage
+
+  def workoutpage
     @ex_routine = Routine.find(1).exercises
     @videos = []
     @ex_routine.each do |v|
       @videos << v.video
     end
   end
+
+
+
 end
