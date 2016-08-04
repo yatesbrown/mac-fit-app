fd347e1dcedb39a843e0cfc37236ced75d005bee
Mac Cassidy <maccassidy1995@gmail.com>
2016-08-04 13:59:19 -0400

    finallllyyyyyy got the buttons to function correctly, also started just switching src with the data-src attr of the videos, so we don't have to load a bunch at once

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -4,10 +4,47 @@
 
 $(document).on('turbolinks:load', function(){
   var incrementor = 1;
-  $('.video-container').first().css("display", "inline");
-  $('#the_button').on("click", function(){
-    $('#v'+incrementor).css("display", "none");
-    $('#v'+(incrementor+1)).css("display", "inline");
-    incrementor++;
+
+
+  $('#the-back-button').css('display', 'none');
+  $('#the-next-button').css('display', 'none');
+
+  $('#start-button').on("click", function(){
+    $('.video-container').first().css('display', "inline");
+    $('#the-next-button').css('display', 'inline');
+    $('#the-back-button').css("display", "none");
+    $('#start-button').css('display', "none");
+    $('#v1').attr('src', $('#v1').attr('data-src'));
   });
+
+
+  $('#the-next-button').on("click", function(){
+    $('#d'+incrementor).css("display", "none");
+    $('#v'+incrementor).removeAttr('src')
+    $('#d'+(incrementor+1)).css("display", "inline");
+    $('#v'+(incrementor+1)).attr('src', $('#v'+(incrementor+1)).attr('data-src'));
+    incrementor++;
+      if(incrementor === $('.video-container').length){
+        $('#the-next-button').css('display', "none");
+      }
+      else {
+        $('#the-back-button').css('display', 'inline');
+      }
+  });
+
+  $('#the-back-button').on("click", function(){
+    $('#d'+incrementor).css("display", "none");
+    $('#v'+incrementor).removeAttr('src');
+    $('#d'+(incrementor-1)).css("display", "inline");
+    $('#v'+(incrementor-1)).attr('src', $('#v'+(incrementor-1)).attr('data-src'));
+    incrementor--;
+      if(incrementor === 1){
+        $('#the-back-button').css('display', "none");
+      }
+      else {
+        $('#the-next-button').css('display', 'inline');
+      }
+  });
+
+
 });
